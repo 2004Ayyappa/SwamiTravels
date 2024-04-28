a95fcce11c4c0a669459bfa675b9a9abb8bbd887
2004Ayyappa <148331579+2004Ayyappa@users.noreply.github.com>
2024-04-28 08:41:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,39 +2,36 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 8080;
-const adminRouter=require("./src/controllers/admin.controller");
+const adminRouter = require("./src/controllers/admin.controller");
 const cityRouter = require("./src/controllers/city.controller");
 const busRouter = require("./src/controllers/bus.controller");
-const userRouter=require("./src/controllers/user.controller");
-const orderRouter=require("./src/controllers/order.controller")
+const userRouter = require("./src/controllers/user.controller");
+const orderRouter = require("./src/controllers/order.controller");
 const paymentController = require('./src/controllers/payment.controller');
-// const cityRoutes = require("./routes/cityRoutes");
 const connect = require("./src/configs/db");
-
-app.use(cors(
-  {
-    origin:["https://swami-travels-client.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
+app.use(cors({
+  origin: ["https://swami-travels-client.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 app.use(express.json());
-app.use("/admin",adminRouter);
-app.use("/user",userRouter);
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 app.use("/city", cityRouter);
 app.use("/bus", busRouter);
 app.use("/order", orderRouter);
 
-//Razorpay payment
+// Razorpay payment
 app.use("/api/payment", paymentController);
 
-// app.use("/cities", cityRoutes);
+// Handle preflight requests
+app.options('*', cors());
 
 app.listen(port, async () => {
   try {
     await connect();
-    console.log(`listening on https://swami-travels.vercel.app`);
-    console.log('Server is running')
+    console.log(`Listening on port ${port}`);
+    console.log('Server is running');
   } catch (error) {
     console.log(error.message);
   }
