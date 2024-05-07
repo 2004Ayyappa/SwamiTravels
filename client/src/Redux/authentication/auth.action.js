68c2ec05a2815f3551901a26c421016f93bf0909
Venigalla Swami Ayyappa <148331579+2004Ayyappa@users.noreply.github.com>
2024-05-07 10:45:13 +0530

    Update auth.action.js

--- client/src/Redux/authentication/auth.action.js
+++ client/src/Redux/authentication/auth.action.js
@@ -10,7 +10,7 @@ import { error, success } from "../../Utils/notification";
 export const loginAPI = (data, navigate) => async (dispatch) => {
  
   try {
-    let response = await axios.post("https://swamitravels-bbxl.onrender.com/user/login", data);
+    let response = await axios.post("https://swamitravels-api.onrender.com/user/login", data);
     // console.log(response);
     if (response.data.status === "Failed") {
       error(response.data.message);
